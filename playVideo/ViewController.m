c0a06a9f4e98953909490524166158ef13adb539
katsukifan <katsukifan@gmail.com>
2019-02-19 17:15:19 +0800

    First commit

--- playVideo/ViewController.m
+++ playVideo/ViewController.m
@@ -7,23 +7,77 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) MPMoviePlayerController *moviePlayer;
+
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // NSString *filePath = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"mp4"];
+    // NSURL *sourceMovieURL = [NSURL fileURLWithPath:filePath];
+    
+    // _moviePlayer = [[MPMoviePlayerController alloc] initWithContentURL:sourceMovieURL];
+    // _moviePlayer.view.frame=CGRectMake(0, 0, 320, 320);
+    // _moviePlayer.controlStyle=MPMovieControlStyleNone;
+    
+    // Play the movie!
+    // [self.moviePlayer play];
+    // [self.view addSubview:_moviePlayer.view];
+    
+    
+
+    
 }
+
+
+- (IBAction)play:(id)sender {
+    
+    self.moviePlayer = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"test" ofType:@"mp4"]]];
+    
+    [self.moviePlayer.view setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:self.moviePlayer];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(movieFinishedCallback:) name:MPMoviePlayerPlaybackDidFinishNotification object:self.moviePlayer];
+    
+    
+    // Play the movie!
+    
+    [self.view addSubview:self.moviePlayer.view];
+    [self.moviePlayer prepareToPlay];
+    [self.moviePlayer play];
+    
+ 
+    
+    
+}
+
+
+- (void)movieFinishedCallback:(NSNotification *) aNotification {
+    
+    [self.moviePlayer stop];
+    [self.moviePlayer.view removeFromSuperview];
+    
+}
+
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 
 @end
